191dbecb241e0c19703d2b8697fddcad55947e83
victor291201 <victor291201hugo@gmail.com>
2023-11-02 23:42:22 -0500

    refactoring of random forest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # Tratamiento de datos
-
-
+import time
 import numpy as np
 import pandas as pd
 import string
@@ -12,6 +11,18 @@ from nltk.corpus import wordnet
 from nltk.stem import WordNetLemmatizer
 from scipy.spatial.distance import cosine
 from unidecode import unidecode
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
 
 # Gráficos
 import matplotlib.pyplot as plt
@@ -41,42 +52,7 @@ df.columns = ["Lenguage", "Text"]
 
 # Verificar que los datos se hayan cargado correctamente
 
-
-# Limpiando el conjunto
-def remove_posE(tokens):
-    # Obtener la lista de stopwords (incluye preposiciones) en inglés
-    stopwords = set(nltk.corpus.stopwords.words('english'))
-
-    pronouns = set()
-    tagged_words = pos_tag(stopwords)
-    for word, tag in tagged_words:
-        if tag.startswith('PRP'):
-            pronouns.add(word)
-
-    tagged_tokens = pos_tag(tokens)
-    tokens_without_pos = [token[0] for token in tagged_tokens if token[0]
-                          not in stopwords and token[0] not in pronouns]
-
-    return tokens_without_pos
-
-
-def remove_posD(tokens):
-    # Obtener la lista de stopwords (incluye preposiciones) en inglés
-    stopwords = set(nltk.corpus.stopwords.words('dutch'))
-
-    pronouns = set()
-    tagged_words = pos_tag(stopwords)
-    for word, tag in tagged_words:
-        if tag.startswith('PRP'):
-            pronouns.add(word)
-
-    tagged_tokens = pos_tag(tokens)
-    tokens_without_pos = [token[0] for token in tagged_tokens if token[0]
-                          not in stopwords and token[0] not in pronouns]
-
-    return tokens_without_pos
-
-
+# limpiando el conjunto de datos
 def limpiar_tokenizar(lang, texto):
     # Crear instancia del lematizador
     lemmatizer = WordNetLemmatizer()
@@ -97,11 +73,6 @@ def limpiar_tokenizar(lang, texto):
     nuevo_texto = re.sub("\d{1,2}/\d{1,2}/\d{2,4}", ' ', nuevo_texto)
     # Tokenización por palabras individuales
     nuevo_texto = nuevo_texto.split(sep=' ')
-    # Eliminar preposiciones, pronombres y stopWords
-    # if (lang == "en"):
-    #    nuevo_texto = remove_posE(nuevo_texto)
-    # else:
-    #    nuevo_texto = remove_posD(nuevo_texto)
     # Lematización
     nuevo_texto = [lemmatizer.lemmatize(token) for token in nuevo_texto]
     # Eliminación de tokens con una longitud < 2
@@ -111,7 +82,7 @@ def limpiar_tokenizar(lang, texto):
 
 
 df['texto_tokenizado'] = df.apply(
-    lambda x: limpiar_tokenizar(x[0], x[1]), axis=1)
+    lambda x: limpiar_tokenizar(x[1]), axis=1)
 df.to_csv(
     'resultadoTokeniz.csv', index=False)
 
@@ -120,3 +91,199 @@ texto_tidy = df.explode(column='texto_tokenizado')
 texto_tidy = texto_tidy.drop(columns='Text')
 texto_tidy = texto_tidy.rename(columns={'texto_tokenizado': 'token'})
 texto_tidy.head(10)
+# leyendo el csv
+
+# Especificar la ruta del archivo CSV
+ruta_archivo = r'.\resultadoTokeniz.csv'
+
+# Leer el archivo CSV
+df = pd.read_csv(ruta_archivo, nrows=100000)
+
+# Acceder a los datos de las columnas
+freshness = df['Lenguage']
+review = df['Text']
+texto_tokenizado = df['texto_tokenizado']
+
+df.head()
+# hallando xtrain, xtext, ytrain, ytest
+
+datos_X = df['texto_tokenizado']
+datos_y = df['Lenguage']
+
+print(datos_X.unique())
+print(datos_y.unique())
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    datos_X,
+    datos_y,
+    test_size=0.80,
+    random_state=42
+)
+
+vectorizer = CountVectorizer()
+
+# Ajustar el vectorizador con los datos de entrenamiento
+Xtrain = vectorizer.fit_transform(X_train)
+
+# Transformar los datos de prueba utilizando el vectorizador ajustado
+Xtest = vectorizer.transform(X_test)
+
+print(Xtrain.shape)
+
+# datos
+print("Conjunto de entrenamiento - Características:", Xtrain.shape)
+print("Conjunto de entrenamiento - Etiquetas:", y_train.shape)
+print("Conjunto de prueba - Características:", Xtest.shape)
+print("Conjunto de prueba - Etiquetas:", y_test.shape)
+
+# implementacion de random forest
+
+modelo_rf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+print("Dimensiones entreamiento :", Xtrain.shape)
+print("Dimensiones prueba ", y_train.shape)
+
+
+rf_Model = RandomForestClassifier()
+cross_val_scores = cross_val_score(rf_Model, Xtrain, y_train, cv=2)
+rf_Model.fit(Xtrain, y_train)
+# Evaluar el rendimiento del modelo en los datos de entrenamiento y prueba
+train_accuracy = rf_Model.score(Xtrain, y_train) * 100
+test_accuracy = rf_Model.score(Xtest, y_test) * 100
+
+print("Precisión del entrenamiento:", "{:.3f}%".format(train_accuracy))
+print("Precisión de prueba:", "{:.3f}%".format(test_accuracy))
+
+
+print("Resultados de la validación cruzada:")
+print(cross_val_scores)
+print("Precisión media: {:.2f}".format(cross_val_scores.mean()))
+
+# Crear el modelo de Random Forest
+modelo_rf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+print("Dimensiones entreamiento :", Xtrain.shape)
+print("Dimensiones prueba ", y_train.shape)
+
+# Número de árboles en el bosque aleatorio
+n_estimators = [int(x) for x in np.linspace(start=150, stop=400, num=4)]
+# Número de características a considerar en cada división
+max_features = ['auto', 'sqrt']
+# Número máximo de niveles en el árbol
+max_depth = None,
+# Número mínimo de muestras requeridas para dividir un nodo
+min_samples_split = [5, 10]
+# Número mínimo de muestras requeridas en cada nodo hoja
+min_samples_leaf = [1, 2]
+# Método de selección de muestras para entrenar cada árbol
+
+param_grid = {'n_estimators': n_estimators,
+              'max_features': max_features,
+              'max_depth': max_depth,
+              'min_samples_split': min_samples_split,
+              'min_samples_leaf': min_samples_leaf,
+              }
+print(param_grid)
+
+rf_Grid = GridSearchCV(
+    estimator=rf_Model, param_grid=param_grid, cv=2, verbose=2, n_jobs=4)
+rf_Grid.fit(Xtrain, y_train)
+
+
+train_accuracy = rf_Grid.score(Xtrain, y_train) * 100
+test_accuracy = rf_Grid.score(Xtest, y_test) * 100
+print("Precisión del entrenamiento:", "{:.3f}%".format(train_accuracy))
+print("Precisión de prueba:", "{:.3f}%".format(test_accuracy))
+
+
+# Crear el modelo de Random Forest
+modelo_rf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+print("Dimensiones :", Xtrain.shape)
+print("Dimensiones ", y_train.shape)
+
+
+# Número de árboles en el bosque aleatorio
+n_estimators = [int(x) for x in np.linspace(start=100, stop=150, num=3)]
+# Número de características a considerar en cada división
+max_features = ['auto', 'sqrt']
+# Número máximo de niveles en el árbol
+max_depth = [2, 4]
+# Número mínimo de muestras requeridas para dividir un nodo
+min_samples_split = [2, 5]
+# Número mínimo de muestras requeridas en cada nodo hoja
+min_samples_leaf = [1, 2]
+param_grid = {
+    'n_estimators': n_estimators,
+    'max_features': max_features,
+    'max_depth': max_depth,
+    'min_samples_split': min_samples_split,
+    'min_samples_leaf': min_samples_leaf,
+}
+
+print(param_grid)
+
+
+rf_Grid = GridSearchCV(
+    estimator=rf_Model, param_grid=param_grid, cv=2, verbose=2, n_jobs=4)
+rf_Grid.fit(Xtrain, y_train)
+
+rf_Grid.best_params_
+{'max_depth': 4,
+ 'max_features': 'auto',
+ 'min_samples_leaf': 1,
+ 'min_samples_split': 5,
+ 'n_estimators': 150}
+# Evaluar el rendimiento del modelo en los datos de entrenamiento y prueba
+train_accuracy = rf_Grid.score(Xtrain, y_train) * 100
+test_accuracy = rf_Grid.score(Xtest, y_test) * 100
+print("Precisión del entrenamiento:", "{:.3f}%".format(train_accuracy))
+print("Precisión de prueba:", "{:.3f}%".format(test_accuracy))
+
+
+param_sets = [
+    {'n_estimators': 30, 'min_samples_split': 15,
+        'min_samples_leaf': 15, 'max_depth': 28, 'max_features': 'auto'},
+]
+
+for i, params in enumerate(param_sets):
+    start_time = time.time()  # Registrar el tiempo de inicio
+
+    # Crear una instancia del modelo RandomForestClassifier
+    modelo_rf = RandomForestClassifier(**params)
+
+    # Entrenar el modelo con los parámetros actuales
+
+    modelo_rf.fit(Xtrain, y_train)
+
+    # Evaluar el rendimiento del modelo (por ejemplo, calcular la precisión)
+    train_accuracy = modelo_rf.score(Xtrain, y_train) * 100
+    test_accuracy = modelo_rf.score(Xtest, y_test) * 100
+
+    end_time = time.time()  # Registrar el tiempo de finalización
+    elapsed_time = end_time - start_time  # Calcular el tiempo transcurrido
+
+    # Redondear el tiempo transcurrido a dos decimales
+    elapsed_time = round(elapsed_time, 2)
+
+    # Imprimir los resultados con el número de prueba, tiempo de ejecución y precisión en %
+    print("Número de prueba:", i+1)
+    print("Parámetros:", params)
+    print("Precisión de entrenamiento:", "{:.2f}%".format(train_accuracy))
+    print("Precisión de prueba:", "{:.2f}%".format(test_accuracy))
+    print("Tiempo de ejecución :", elapsed_time, "segundos")
+    print("--------------------------------------")
+
+    # Calcular la matriz de confusión
+    y_pred = modelo_rf.predict(Xtest)
+    cm = confusion_matrix(y_test, y_pred)
+
+    labels = np.unique(y_test)
+    df_cm = pd.DataFrame(cm, index=labels, columns=labels)
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(df_cm, annot=True, cmap='Blues', fmt='d')
+    plt.title("Matriz de Confusión")
+    plt.xlabel("Etiqueta Predicha")
+    plt.ylabel("Etiqueta Verdadera")
+    plt.show()
